7818b55867fb750a78c4408efc9bd8830b1a5596
Mustafa Elnagar <mus.elnagar@gmail.com>
2018-03-17 18:26:27 +0800

    updating the licence and Thank you message.
    updating the licence and Thank you message.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Wordpress-plugin-Reading-indicator
 Reading Indicator plugins enables the user to see how much he read from the blog and how long still to read by a very interactive indicator at the bottom of the single blogs.
+
+Thanks to Rames, Cheeling, leonid, CSS-Tricks website, who are inspiring me to back to coding & the open source community again.
+
--- Wordpress-plugin-Reading-indicator.php
+++ Wordpress-plugin-Reading-indicator.php
@@ -5,9 +5,8 @@
  * Description: Reading Indicator plugins enables the user to see how much he read from the blog and how long still to read by a very interactive indicator at the bottom of the single blogs. Thanks to Rames, Cheeling, leonid, CSS-Tricks website, who are inspiring me to back to coding & the open source community again.
  * Version: 1.0
  * Author: Mustafa Elnagar
- * Author URI: https://mustafaelnagar.com
- * License: GBL
- * Copyright 2018 
+ * Author URI: http://mustafaelnagar.com
+ * License: GPLv2 
  */
 
 defined( 'ABSPATH' ) || die();
